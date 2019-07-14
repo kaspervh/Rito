63fd11e91d2e1a85a170c1d8a734e4bd70110d06
kaspervh <kaspervhauschildt@protonmail.com>
2019-07-14 20:38:00 +0200

    allmost done with the sorting

--- edit.php
+++ edit.php
@@ -8,9 +8,21 @@
 
   $productDescriptions = getProductDescription($id);
 
-  $languages = getLanguages();
+  if(isset($_POST['submit'])){
 
-  #var_dump($productDescriptions)
+  }
+
+  $arrays = []; 
+  foreach (getLanguages() as $language) {
+    $arr = ['languages_id' => $language['languages_id'], 'products_id' => $_POST['products_id']];
+    foreach ($_POST as $post_name => $post) {
+      if(strstr($post_name, '_', true) === $language['languages_name']){
+        $arr += [$post_name=> $post];
+      }
+    }
+    var_dump($arr);
+  }
+  
 ?>
 
 
@@ -19,41 +31,42 @@
   <a href="<?php echo root_path; ?>" class="btn btn-primary" style="margin: 15px"> <-- Tilbage</a>
     
     <span >
-      <form action="POST" style="margin: 15px;">
+      <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>" style="margin: 15px;">
         <label>Varenummer</label>
         <br>
-        <input type="text" value="<?php echo $product['products_reference']; ?>">
+        <input type="text" name="products_reference" value="<?php echo $product['products_reference']; ?>">
         <br>
         <label>Pris</label>
         <br>
-        <input type="string" value="<?php echo $product['products_price'] ?>">
+        <input type="string" name="products_price" value="<?php echo $product['products_price']; ?>">
         <br>
         <br>
         
-        <?php foreach ($productDescriptions as $productDescription): ?>
-          <div class="d-md-inline-flex">
+        <div class="d-md-inline-flex">
+          <?php foreach ($productDescriptions as $productDescription): ?>
             <div class="card" style="margin: 5px;">
-              
+              <?php echo $productDescription['languages_id'] ?>
               <strong>Sprog: <?php echo $productDescription['languages_name'] ?></strong>
               <br>
               <div class="d-flex p-2 ">
-                <input type="hidden" id='products_id' value="<?php echo $product['products_id'] ?>">
-                <input type="hidden" id="languages_id" value="<?php echo $productDescription['languages_id'] ?>">
+                <input name="products_id" value="<?php echo $product['products_id']; ?>" style="display: none;">
+                <input name="languages_name" value="<?php echo $productDescription['languages_name']; ?>" style="display: none;" >
                 
                 <label>Varenavn</label>
-                <input type="string" value="<?php echo $productDescription['products_description_name'] ?>">
+                <input type="string" name="<?php echo "{$productDescription['languages_name']} products_description_name"; ?>" value="<?php echo $productDescription['products_description_name']; ?>">
               </div>
 
               <label>Korttekst</label>
-              <textarea name="billy" id="1" rows="5"><?php echo $productDescription['products_description_short_description']; ?></textarea>
+              <textarea name="<?php echo "{$productDescription['languages_name']} products_short_description"; ?>" rows="5"><?php echo $productDescription['products_description_short_description']; ?></textarea>
 
               <label>Langtekst</label>
-              <textarea name="Billy" id="1" cols="30" rows="10"><?php ECHO $productDescription['products_description_description'] ?></textarea>
+              <textarea name="<?php echo "{$productDescription['languages_name']} products_description_description"; ?>" cols="30" rows="10"><?php echo $productDescription['products_description_description']; ?></textarea>
             </div>
 
             
           <?php endforeach; ?>
-        </div>
+      </div>
+        <input type="submit" name="submit"  class="btn-primary" style="float: right; margin-right: 75px; margin-bottom: 50px">
       </form>
     </span>
 
--- new_language.php
+++ new_language.php
@@ -1,11 +1,27 @@
 <?php 
   require('styles/header.php'); 
   require('config/routes.php');
-  require('config/queries.php')
+  require('config/queries.php');
+
+  //check for submit
+  if(isset($_POST['submit'])){
+    if(empty($_POST['name']) && $_POST['name'] == ''){
+      set_error_handler($error = 'Please write a name');
+      echo $error;
+    }else{
+      var_dump($_POST);
+    }
+  }
+  
 ?>
 
 <h1>Tilføj nyt sprog</h1>
 
-<form action="POST"></form>
+<form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
+  <label>Navn:</label>
+  <input type="text" name="name">
+  <br>
+  <input type="submit" name="submit" value="Gem" class="btn btn-primary">
+</form>
 
 <?php require('styles/footer.php'); ?>
